54dca46cdd4105cfc703be9b0e5e7f674d014e9b
Rustom Jehangir <rustyj110@gmail.com>
2015-05-29 16:28:02 -0700

    Changed the I2C throttle command to accept an int16 instead of uint16 and use the sign ofthe int to determine direction instead of the MSB. The 15 LSBs needed to be inverted as well. This is working well and accepting commands in the full range from -32767 to 32767.

--- tgy.asm
+++ tgy.asm
@@ -2303,19 +2303,29 @@ evaluate_rc_i2c:
 		rjmp	rc_do_scale		; The rest of the code is common
 	.else
 		cbr	flags1, (1<<EVAL_RC)+(1<<REVERSE)
-		.if MOTOR_REVERSE
+		;.if MOTOR_REVERSE
 		sbrc	YH, 7
-		.else
-		sbrs	YH, 7
-		.endif
-		sbr	flags1, (1<<REVERSE)
-		cbr	YH, (1<<7)
+		;.else
+		;sbrs	YH, 7
+		;.endif
+		rcall i2c_set_reverse
+		;sbr	  flags1, (1<<REVERSE)
+		;cbr	YH, (1<<7)
 		adiw	YL, 0			; 16-bit zero-test
 		breq	rc_duty_set		; Power off
 	; Scale so that Y == 32767 is MAX_POWER.
 		movw	temp1, YL
 		ldi2	temp3, temp4, 0x10000 * (POWER_RANGE - MIN_DUTY) / 32767
 		rjmp	rc_do_scale		; The rest of the code is common
+i2c_set_reverse:
+		sbr	  flags1, (1<<REVERSE)
+		; The next three lines make the value positive so that we basically
+		; set the reverse flag and then absolute value the speed
+		com   YH
+		com   YL
+		subi  YL,-1
+		sbci  YH,-1
+		ret
 	.endif
 .endif
 ;-----bko-----------------------------------------------------------------
